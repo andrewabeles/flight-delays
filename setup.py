afec24380e56961cccd1a0259294d60f2daba1c8
andrewabeles <38055336+andrewabeles@users.noreply.github.com>
2022-09-23 11:09:35 -0700

    Create project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='2021 U.S. domestic flight delay analysis.',
+    author='Andrew Abeles',
+    license='',
+)
