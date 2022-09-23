4f1582666896dbb08a5ad0891d8ae2a3a32e9dc0
andrewabeles <38055336+andrewabeles@users.noreply.github.com>
2022-09-23 16:10:20 -0700

    Add unzip flight files to make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,6 +3,7 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+from src.data.processing import unzip_flight_files
 
 
 @click.command()
@@ -15,6 +16,11 @@ def main(input_filepath, output_filepath):
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
+    logger.info('unzipping raw flight files')
+    input_dir = '{0}/flights'.format(input_filepath)
+    unzip_flight_files(input_dir, 'data/interim/flights')
+
+    logger.info('done')
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
