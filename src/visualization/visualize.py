c3849b48834dd42d802663368c1e7c8462dd86b8
andrewabeles <38055336+andrewabeles@users.noreply.github.com>
2022-10-12 23:43:44 -0700

    Only plot columns with NAs

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def plot_na_by_col(df, **kwargs):
-    na_by_col = df.isna().sum().compute() / len(df) 
+    cols_with_na = df.columns[df.isna().any()]
+    na_by_col = df[cols_with_na].isna().sum().compute() / len(df) 
     na_by_col.sort_values(ascending=False, inplace=True)
     fig, ax = plt.subplots(**kwargs)
     ax = sns.barplot(
